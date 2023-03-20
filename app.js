d6b6d04b9932d608c98e44022b08ddff3d201594
Banr <61343370+B4nr@users.noreply.github.com>
2023-03-20 12:38:06 +0000

    0.1.1

---
+++ app.js
@@ -0,0 +1,11 @@
+function goToCalc (){
+    window.location.href = "calculator.html";
+}
+
+function goToAgeCalc (){
+    window.location.href = "ageCalc.html";
+}
+
+function goToMain () {
+window.location.href = "index.html";
+}
