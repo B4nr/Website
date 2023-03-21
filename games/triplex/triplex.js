e52410e08f3281d7b51e0d5e415f600dcb841c3b
Banr <61343370+B4nr@users.noreply.github.com>
2023-03-21 00:35:59 -0300

    0.1.44
    Doing some changes. Javascript submit button not working properly :(

--- games/triplex/triplex.js
+++ games/triplex/triplex.js
@@ -1,3 +1,9 @@
+var sum = 3;
+var prod = 1;
+let text = `Sum: ${sum} && Product: ${prod};`;
+localStorage.setItem("textKey", text);
+
+
 function goToMain () {
     window.location.href = "../../index.html";
 }
@@ -5,4 +11,10 @@ function goToMain () {
 function gameOver () {
     alert("You Lost BOZO!")
 
+}
+
+var someVarName = localStorage.getItem("someVarKey");
+
+function writeVariables () {
+    document.getElementById("vars").innerHTML = localStorage.getItem("textKey");
 }
