33a0400ba50eb837580ef9b105a837214ae3b4c2
Banr <61343370+B4nr@users.noreply.github.com>
2023-03-21 13:26:01 -0300

    0.1.45
    Getting random codes based on the difficulty the user is currently in. Main part of this commit was studying the old cpp code because i needed to understand how the game worked in the first place.
    
    Finished this commit while listening to the Furia vs Fnatic EPL in the Gaules stream.

--- games/triplex/triplex.js
+++ games/triplex/triplex.js
@@ -1,5 +1,4 @@
-var sum = 3;
-var prod = 1;
+var level = 1;
 let text = `Sum: ${sum} && Product: ${prod};`;
 localStorage.setItem("textKey", text);
 
@@ -17,4 +16,17 @@ var someVarName = localStorage.getItem("someVarKey");
 
 function writeVariables () {
     document.getElementById("vars").innerHTML = localStorage.getItem("textKey");
+}
+
+// This function is going to return the codes based on the difficulty level.
+function getCode (difficultyLevel) {
+    var codeA, codeB, codeC;
+    codeA = Math.floor(Math.random() * difficultyLevel)+1;
+    codeB = Math.floor(Math.random() * difficultyLevel)+1;
+    codeC = Math.floor(Math.random() * difficultyLevel)+1;
+    return [codeA, codeB, codeC];
+}
+
+function difficultyLvl () {
+    level = level + 1;
 }
